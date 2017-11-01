1eb667d1203bb7ab1ac8cfb03f1e0acfa8a9d1c1
Grant Smith <smith9327@yahoo.com>
2017-10-31 22:12:39 -0500

    First Commit + Finished IStack,LinkedStack,ReverseLines,TestLinkedStack

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -5,19 +5,16 @@ import java.util.Scanner;
 public class ReverseLines {
 
   public static void main(final String[] args) {
-    // TODO read successive input lines until EOF, then print out in reverse order
-
-
-
     final Scanner input = new Scanner(System.in);
+    printReverse(input);
+  }
+
+  private static void printReverse(final Scanner input) {
+    // recursively read and print successive input lines until EOF, then print them out in reverse order
     while (input.hasNextLine()) {
-      final String line = input.nextLine();
-
-
-
+      String data = input.nextLine();
+      printReverse(input);
+      System.out.println(data);
     }
-
-
-
   }
 }
